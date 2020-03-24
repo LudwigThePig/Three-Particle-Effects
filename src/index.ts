c48b127cee0112a1bb87cb867b3ebf409781bed1
Morgan Galvin <morgan@ludwigthepig.com>
2020-03-23 19:07:13 -0600

    rfc: move typing from interface to class implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as THREE from 'three';
 import { randomBoundedInt, randomBoundedFloat } from './utils/random';
 import ConeShape from './shapes/ConeShape';
-import { IParticleSystem, IParticleOptions } from './types';
+import { IParticleSystem, IParticleOptions, vectorRange, color, IShape } from './types';
+import { Object3D } from 'three';
 
-const defaultOptions: IParticleOptions = {
+const defaultOptions = {
   initialRotationRange: [
     // one of tuple of vec3<float> or vec3<float>. Values in radians
     new THREE.Vector3(0, 0, 0),
@@ -23,32 +24,53 @@ const defaultOptions: IParticleOptions = {
   shape: new ConeShape(),
 };
 
-export default class ParticleSystem implements IParticleSystem {
-  constructor(target, options: IParticleOptions = {}) {
+export default class ParticleSystem {
+  color: color = 0xedaa67;
+  initialRotationRange: vectorRange = [
+    // one of tuple of vec3<float> or vec3<float>. Values in radians
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(Math.PI * 2, Math.PI * 2, Math.PI * 2),
+  ];
+  loop: boolean = true;
+  maxParticles: number = 100;
+  particleLifetime: number = 2000; // in MS
+  particlesPerSecond: number = 50;
+  particleVelocity: number = 1; // units per second
+  rotationRate: number = 0; // in radians
+  radius: THREE.Vector3 = new THREE.Vector3(1, 1, 1);
+  minParticleSize: number = 0.1;
+  maxParticleSize: number = 0.1;
+  playOnLoad: boolean = true;
+  shape: IShape = new ConeShape();
+  target: Object3D;
+
+  particleQueue: Array<Object3D> = [];
+  isPlaying: boolean = true;
+  elapsedTime: number = 0;
+  startTime: number;
+
+  constructor(target: Object3D, options: IParticleOptions = {}) {
     // User Defined Values
-    options = { ...defaultOptions, options };
-    this.color = options.color;
-    this.initialRotationRange = options.initialRotationRange;
-    this.loop = options.loop;
-    this.minParticleSize = options.minParticleSize || options.maxParticleSize || 0.1;
-    this.maxParticles = options.maxParticles;
+    this.color = options.color || this.color;
+    this.initialRotationRange = options.initialRotationRange || this.initialRotationRange;
+    this.loop = options.loop || this.loop;
+    this.minParticleSize = options.minParticleSize || options.maxParticleSize || this.minParticleSize;
+    this.maxParticles = options.maxParticles || this.minParticleSize;
     this.maxParticleSize = options.maxParticleSize || options.minParticleSize || 0.1;
-    this.particleLifetime = options.particleLifetime;
-    this.particlesPerSecond = options.particlesPerSecond;
-    this.particleVelocity = options.particleVelocity;
-    this.particleQueue = [];
-    this.play = options.playOnLoad;
-    this.radius = options.radius;
-    this.rotationRate = options.rotationRate;
+    this.particleLifetime = options.particleLifetime || this.particleLifetime;
+    this.particlesPerSecond = options.particlesPerSecond || this.particlesPerSecond;
+    this.particleVelocity = options.particleVelocity || this.particleVelocity;
+    this.isPlaying = options.playOnLoad || this.isPlaying;
+    this.radius = options.radius || this.radius;
+    this.rotationRate = options.rotationRate || this.rotationRate;
     this.target = target;
-    this.shape = options.shape;
+    this.shape = options.shape || this.shape;
 
     // Member Variables
-    this.elapsedTime = 0;
     this.startTime = Date.now();
   }
 
-  createPaticle() {
+  createPaticle(): void {
     const size = randomBoundedInt(this.minParticleSize, this.maxParticleSize);
     const geometry = new THREE.BoxBufferGeometry(size, size, size);
     const material = new THREE.MeshBasicMaterial({ color: this.color });
@@ -58,20 +80,16 @@ export default class ParticleSystem implements IParticleSystem {
     newParticle.position.y = randomBoundedFloat(-this.radius.y, this.radius.y);
     newParticle.position.z = randomBoundedFloat(-this.radius.z, this.radius.z);
 
-    if (Array.isArray(this.initialRotationRange)) {
-      newParticle.rotation.x = randomBoundedFloat(this.initialRotationRange[0].x, this.initialRotationRange[1].x);
-      newParticle.rotation.y = randomBoundedFloat(this.initialRotationRange[0].y, this.initialRotationRange[1].y);
-      newParticle.rotation.z = randomBoundedFloat(this.initialRotationRange[0].z, this.initialRotationRange[1].z);
-    } else {
-      newParticle.rotation = this.initialRotationRange;
-    }
+    newParticle.rotation.x = randomBoundedFloat(this.initialRotationRange[0].x, this.initialRotationRange[1].x);
+    newParticle.rotation.y = randomBoundedFloat(this.initialRotationRange[0].y, this.initialRotationRange[1].y);
+    newParticle.rotation.z = randomBoundedFloat(this.initialRotationRange[0].z, this.initialRotationRange[1].z);
 
     this.target.add(newParticle);
     this.particleQueue.push(newParticle);
   }
 
-  update(deltaTime = 0.02 /* 50fps */) {
-    if (!this.play) return;
+  update(deltaTime: number = 0.02 /* 50fps */): void {
+    if (!this.isPlaying) return;
     if (!this.loop && this.elapsedTime > this.particleLifetime) {
       this.stop();
       return;
@@ -97,20 +115,20 @@ export default class ParticleSystem implements IParticleSystem {
     this.elapsedTime = Date.now() - this.startTime;
   }
 
-  play() {
-    this.play = true;
+  play(): void {
+    this.isPlaying = true;
   }
 
-  pause() {
-    this.play = false;
+  pause(): void {
+    this.isPlaying = false;
   }
 
-  clear() {
+  clear(): void {
     this.target.remove(...this.particleQueue);
     this.particleQueue = [];
   }
 
-  stop() {
+  stop(): void {
     this.pause();
     this.clear();
   }
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,10 @@
 export interface IShape {}
 
+export type vectorRange = [THREE.Vector3, THREE.Vector3];
+export type color = number | string;
+
 export interface IParticleOptions {
-  initialRotationRange?: [THREE.Vector3, THREE.Vector3];
+  initialRotationRange?: vectorRange;
   maxParticles?: number;
   particleLifetime?: number;
   particlesPerSecond?: number;
@@ -10,32 +13,8 @@ export interface IParticleOptions {
   radius?: THREE.Vector3;
   minParticleSize?: number;
   maxParticleSize?: number;
-  color?: number | string; // Hexadecimal RGB
+  color?: color; // Hexadecimal RGB
   playOnLoad?: boolean;
   loop?: boolean;
   shape?: IShape;
-}
-
-export interface IParticleSystem extends IParticleOptions {
-  new (target: THREE.Object3D, options: IParticleOptions): IParticleSystem;
-  color?: number | string; // Hexadecimal RGB
-  initialRotationRange?: [THREE.Vector3, THREE.Vector3];
-  maxParticles?: number;
-  particleLifetime?: number;
-  particlesPerSecond?: number;
-  particleVelocity?: number;
-  rotationRate?: number;
-  radius?: THREE.Vector3;
-  minParticleSize?: number;
-  maxParticleSize?: number;
-  playOnLoad?: boolean;
-  loop?: boolean;
-  shape?: IShape;
-
-  createPaticle(): void;
-  update(deltaTime?: number): void;
-  play(): void;
-  pause(): void;
-  clear(): void;
-  stop(): void;
 }
