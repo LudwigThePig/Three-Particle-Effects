e671dc71cad64d1347d7402c860e9749416e76dc
Morgan Galvin <morgan@ludwigthepig.com>
2020-03-29 18:53:28 -0600

    chore: initialize sub directory

---
+++ demo/webpack.config.js
@@ -0,0 +1,15 @@
+require('dotenv');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'js'),
+  },
+};
