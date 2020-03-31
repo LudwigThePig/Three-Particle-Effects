01f58ab0c8aac943bb9cb44e4e182d123b609c27
Morgan Galvin <morgan@ludwigthepig.com>
2020-03-30 21:27:49 -0600

    style: add a numeric display

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,22 @@
 import { particles } from './scene';
 
+const $ = (query) => document.querySelector(query); // this feels so wrong
+
 const btn = document.getElementById('toggle-options');
 const ctr = document.getElementById('options-ctr');
-const particleVelocity = document.getElementById('particle-velocity');
+const particleVelocityInput = $('#particle-velocity input[type="range"]');
+const particleVelocityDisplay = $('#particle-velocity .display');
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
   ctr.classList.toggle('active');
 });
 
-particleVelocity.addEventListener('change', (e) => {
-  e.preventDefault();
-  particles.particleVelocity = e.target.value;
+particleVelocityInput.value = particles.particleVelocity;
+particleVelocityDisplay.innerHTML = particles.particleVelocity;
+particleVelocityInput.addEventListener('change', (e) => {
+  const { value } = e.target;
+  console.log(particles);
+  particleVelocityDisplay.innerHTML = value;
+  particles.particleVelocity = value;
 });
