a60a4759f85f1f10091fff59430e662397d54efb
Morgan Galvin <morgan@ludwigthepig.com>
2020-03-30 21:10:33 -0600

    feat: implement first slider into particle-system

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,1 +1,15 @@
 import { particles } from './scene';
+
+const btn = document.getElementById('toggle-options');
+const ctr = document.getElementById('options-ctr');
+const particleVelocity = document.getElementById('particle-velocity');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  ctr.classList.toggle('active');
+});
+
+particleVelocity.addEventListener('change', (e) => {
+  e.preventDefault();
+  particles.particleVelocity = e.target.value;
+});
