e5896daad511d1d49ba1bbd5f5d211bd79100e6d
Morgan Galvin <morgan@ludwigthepig.com>
2020-05-01 22:19:01 -0600

    feat: create random vertex generator util

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,4 @@
+import { Vector3 } from 'three';
 export const randomBool = (): boolean => Math.random() > 0.5;
 
 export const randomArrayItem = (arr: Array<any>): any => {
@@ -10,4 +11,11 @@ export const randomBoundedFloat = (min: number, max: number, precision: number =
   const rand = Math.random() * (max - min) + min;
   const power = Math.pow(10, precision);
   return Math.floor(rand * power) / power;
+};
+
+export const randomBoundedVec3 = (vecA: Vector3, vecB: Vector3): Vector3 => {
+  const x = randomBoundedFloat(vecA.x, vecB.x);
+  const y = randomBoundedFloat(vecA.y, vecB.y);
+  const z = randomBoundedFloat(vecA.z, vecB.z);
+  return new Vector3(x, y, z);
 };
